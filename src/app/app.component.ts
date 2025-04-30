535046728c722b84c18117a05c027a47437afae8
Carla <144458902+privycarla@users.noreply.github.com>
2025-05-01 02:32:17 +0800

    add all files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,22 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  // imports: [RouterOutlet],
+  template: `
+  <div class="max-w-xs min-h-screen mx-auto bg-orange-50 relative overflow-hidden shadow-lg rounded-lg p-4">
+  <img 
+    class="w-full rounded-full mt-10 mb-6 animate-pulse" 
+    src="/assets/ai.png" 
+    alt="Appointment Visual"
+  />
+
+  <div class="absolute bottom-10 w-full px-4">
+    <button class="w-full bg-yellow-600 hover:bg-yellow-700 text-white text-lg font-semibold py-3 rounded-full transition duration-300 shadow-md hover:shadow-lg">
+      BOOK AN APPOINTMENT
+    </button>
+  </div>
+</div>
+`,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
